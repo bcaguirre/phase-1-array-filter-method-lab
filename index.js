288b67675866007697e53517a5cc937fdb557b53
bernadette <aguirre.bernadette@outlook.com>
2022-03-31 20:08:19 -0600

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution here
+function findMatching(arr, str){
+    const result = arr.filter(function(driver){
+       return (driver.toUpperCase() === str.toUpperCase())
+    });
+    return result
+}
+
+function fuzzyMatch(driverName, matchingName){
+   function isMatchingLetters(driverName){
+        return driverName.startsWith(matchingName)
+    }
+    return driverName.filter(isMatchingLetters)    
+}
+
+function matchName(driverInfo, matchingDriver){
+   const result = driverInfo.filter(function(driver){
+    return (driver.name.toLowerCase() === matchingDriver.toLowerCase())
+   });
+   return result
+}
